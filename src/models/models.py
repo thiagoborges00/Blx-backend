0198189cdd618ccc3253ba5f89ac6a38e3a9dd66
Thiago Borges <thiago.borges@armazemparaiba.com.br>
2022-10-10 11:42:57 -0300

    add models

---
+++ src/models/models.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+from pydantic import BaseModel, Pedido, Produto
+
+class Usuario(BaseModel):
+   id:Optional[str] = None
+   nome:str
+   telefone:str
+   #senha:str 
+   minhas_vendas:List[Pedido]
+   meus_produtos:List[Produto]
+   meus_pedidos:List[Pedido]   
+
+class Produto(BaseModel):
+    id:Optional[str]
+    nome:str
+    detalhamento:str
+    preco:float
+    disponivel:bool = False
+    usuario:Usuario
+
+
+class Pedido(BaseModel):
+    id:Optional[str]
+    quantidade:int
+    endereco_entrega:str
+    entrega:bool = True
+    usuario:Usuario
+    produto:Produto
+    observacoes:Optional[str] = "Sem observações"
+
+
+
